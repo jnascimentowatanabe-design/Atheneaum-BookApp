c06d16ae1a58dc8fc4f44ce023cd046b0d30f45f
jnascimentowatanabe-design <jnascimentowatanabe@gmail.com>
2026-09-14 15:56:17 -0300

    Delete BookStatusRecord.java

--- BookStatusRecord.java
+++
@@ -1,7 +0,0 @@
-package Project_1.BookApp.Record;
-
-import com.google.gson.annotations.SerializedName;
-import java.util.List;
-
-public record BookStatusRecord(String title, List<String> authorName, int firstPublishYear) {
-}
